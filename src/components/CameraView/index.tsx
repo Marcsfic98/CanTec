a7fe184ea8605657f753db57a1ddd608e3cfc6b0
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-07-01 17:38:09 -0300

    modal criada

--- src/components/CameraView/index.tsx
+++ src/components/CameraView/index.tsx
@@ -1,12 +1,18 @@
 import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
-import { Button,  Text, TouchableOpacity, View } from 'react-native';
+import { useState , useRef } from 'react';
+import { Button,  Text, TouchableOpacity, View , Modal,Image} from 'react-native';
 import {styles} from './styles'
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { height, width } from '@fortawesome/free-brands-svg-icons/fa42Group';
 export default function CameraV() {
   const [facing, setFacing] = useState<CameraType>('back');
   const [permission, requestPermission] = useCameraPermissions();
+
+  const [capturedPhoto , setCapturedPhoto]= useState<string | null>()
+  const [modalIsOpen , setModalIsOpen] = useState<boolean>(false)
+
+  const camRef = useRef<CameraView>(null)
 
   if (!permission) {
     return <View />;
@@ -26,12 +32,17 @@ export default function CameraV() {
   }
 
   async function takePicture() {
-    console.log("tirou foto")
+    if(camRef && camRef.current){
+        const data = await camRef.current.takePictureAsync();
+
+        setCapturedPhoto(data.uri)
+        
+    }
   }
 
   return (
     <View style={styles.container}>
-      <CameraView ratio='16:9' style={styles.camera} facing={facing}>
+      <CameraView ratio='16:9' style={styles.camera} facing={facing} ref={camRef}>
         <View style={styles.buttonContainer}>
 
                  <TouchableOpacity style={styles.button} onPress={takePicture}>
@@ -43,6 +54,13 @@ export default function CameraV() {
                     <FontAwesome6  name="arrows-rotate" size={40} color="white" />
                 </TouchableOpacity>
                     
+                {capturedPhoto && (
+                    <Modal animationType='slide' transparent={false} visible={modalIsOpen}>
+                        <View style={{flex:1,justifyContent:"center",alignItems:"center",margin:20}}>
+                            <Image style={{width:"100%",height:300,borderRadius:20}} source={{uri:capturedPhoto}}></Image>
+                        </View>
+                    </Modal>  
+                )}  
               
         </View>
       </CameraView>
